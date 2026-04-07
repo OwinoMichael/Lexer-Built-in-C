8b02f63f5ce702fceb59b65862424253374904cd
OwinoMichael <owinomichael1400@gmail.com>
2026-04-07 16:49:53 -0400

    Init function and readChar function for the lexer

--- lexer.c
+++ lexer.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
+#include <string.h>
 #include "lexer.h"
 
 typedef struct SLexer{
-    const char * input; //line of code
-    size_t input_len; //
-    size_t position;
-    size_t readPosition;
-    char ch;
-} Token;
+    const char* input; // Source code
+    size_t input_len; 
+    size_t position; // Current position in input
+    size_t readPosition; // Next position to read
+    char ch; // Current character
+} Lexer;
+
+Lexer* New(const char* input){
+    size_t len = sizeof(input);
+    Lexer* l = malloc(len);
+    memset(l, 0, len);
+
+    l->input = input;
+    l->input_len = len;
+    l->position = 0;
+    l->readPosition = 0;
+    l->ch = NULL;
+
+    return l;
+}
+
+void readChar(Lexer* l){
+    if(l->readPosition >= l->input_len){
+        l->ch = '\0';
+    }else{
+        l->ch = l->input[l->readPosition];
+    }
+
+    l->position = l->readPosition;
+    l->readPosition++;
+}
